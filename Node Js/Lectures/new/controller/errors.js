d85b9ed16246242b558b1d63e188298c7bd8dbc5
AbhiShp <developerabhi17@gmail.com>
2024-12-05 16:04:05 +0530

    fixed

--- Node Js/Lectures/new/controller/errors.js
+++
@@ -1,3 +0,0 @@
-exports.pageNotFound = (req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404', tab:"home"})
-}
--- Node Js/Lectures/new/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./views/**/*.{html,js,ejs}','./views/partials/*.{html,js,ejs}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
