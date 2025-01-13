336fc3c7f4be3e8f73e4887157c8d054c5f2cea0
devphantom7 <abhisingh6268644194@gmail.com>
2025-01-13 23:56:43 +0530

    basics of java

---
+++ Java/Basics/variables.java
@@ -0,0 +1,7 @@
+public class variables {
+    public static void main(String args[]){
+        String name = "Abhi Singh";
+        int age = 20;
+        System.out.print("my name is "+ name+" and my age is "+age);
+    }
+}
