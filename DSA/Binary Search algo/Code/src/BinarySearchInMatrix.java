3c2d07e4f53e83d48ab70ec916d45c42cd1c3098
devphantom7 <abhisingh6268644194@gmail.com>
2025-03-04 21:31:03 +0530

    binary search in 2d arrays

---
+++ DSA/Binary Search algo/Code/src/BinarySearchInMatrix.java
@@ -0,0 +1,16 @@
+public class BinarySearchInMatrix {
+    public static void main(String[] args) {
+        int[][] matrix = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+
+    }
+    static int[] int search(int[][] matrix, int target){
+        int start = 0;
+        int end = matrix.length - 1;
+
+    }
+}
