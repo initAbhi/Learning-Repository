221ad271bfc87b4d672318935c7c50e94dd0ae97
devphantom7 <developerabhi17@gmail.com>
2025-03-22 22:12:19 +0530

    drive clone

--- NextJs/Projects/nextjs-google-drive/src/app/page.js
+++ NextJs/Projects/nextjs-google-drive/src/app/page.js
@@ -1,14 +1,23 @@
+"use client"
 import Image from "next/image";
 import Header from "@/components/Header";
 import Dashboard from "@/components/dashboard";
 import Sidebar from "@/components/Sidebar";
+import { useState } from "react";
 
 export default function Home() {
+  const [assets, setAssets] = useState([])
+  console.log(assets)
+
+  const onHandleNewUpload = (asset) => {
+    setAssets(prev => [asset, ...prev])
+  }
+
   return (
     <div >
       <Header />
       <div className="main-container">
-        <Sidebar />
+        <Sidebar onHandleNewUpload={onHandleNewUpload} />
         <Dashboard />
         {/* yes */}
       </div>
--- NextJs/Projects/nextjs-google-drive/src/components/Sidebar.jsx
+++ NextJs/Projects/nextjs-google-drive/src/components/Sidebar.jsx
@@ -1,10 +1,20 @@
 import { CldUploadWidget } from 'next-cloudinary'
 import React from 'react'
 
-const Sidebar = () => {
+const Sidebar = ({onHandleNewUpload}) => {
   return (
   <article className="side-bar">
-    {/* <CldUploadWidget/> */}
+    <CldUploadWidget uploadPreset='drive_clone' onSuccess={(result) => {
+        console.log(result)
+        onHandleNewUpload(result.info)
+    }} onQueuesEnd={(result, {widget}) => {
+        widget.close()
+    }} >{({open}) => {
+        function handleOnClick() {
+            open()
+        }
+        return (<button onClick={handleOnClick} className='new-button'>New</button>)
+    }}</CldUploadWidget>
     <ul>
         <li>Home</li>
         <li>Activity</li>
